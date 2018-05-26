7394d80685cfaf1be9b4381b92c664f6ffa7d41d
deepgpatel <deepgpatel93@gmail.com>
2018-05-26 14:59:15 -0400

    associations

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,2 +1,4 @@
 class Shoe < ApplicationRecord
+  has_many :shoe_stores
+  has_many :store, through: :shoe_stores
 end
--- app/models/shoe_store.rb
+++ app/models/shoe_store.rb
@@ -1,2 +1,4 @@
 class ShoeStore < ApplicationRecord
+  belongs_to :shoe
+  belongs_to :store
 end
